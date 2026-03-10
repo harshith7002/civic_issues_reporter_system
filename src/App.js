13ae5f2c0619946d92abdb88d06c2cabe85474d1
Jay Mukati <jaymukati@icloud.com>
2026-03-10 10:16:51 +0530

    Refactor App component layout
    Refactor App component structure and remove Router.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from "react";
 import Navbar from "./components/navbar";
 import AppRoutes from "./routes/AppRoutes";
 import "./styles/global.css";
 
-function App(){
-  return(
+function App() {
+  return (
     <>
-      <Navbar/>
-      <AppRoutes/>
+      <Navbar />
+      {/* We removed <Router> from here because it's already in index.js */}
+      <main className="app-container">
+        <AppRoutes />
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
